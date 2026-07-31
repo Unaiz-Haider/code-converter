f4f289d1759697de5a113bf81730671e5a5b2ae9
Unaiz Haider <unaizhaide13512r@gmail.com>
2026-07-31 12:31:34 +0530

    OpenAI 4o mini  API added

--- Frontend/src/components/02_code_section/02.1_CodeLangSelect.jsx
+++ Frontend/src/components/02_code_section/02.1_CodeLangSelect.jsx
@@ -53,6 +53,7 @@ function CodeLangSelect({ selectedLang, setSelectedLang }) {
             <div className="pointer-events-none absolute inset-y-0 right-3 flex items-center text-gray-300">
                 ▼
             </div>
+            
         </div>
     )
 }
