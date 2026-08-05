2a53cc91a3077bcb162cb735dd8f6a910ab99cb4
Unaiz Haider <unaizhaide13512r@gmail.com>
2026-08-05 22:18:20 +0530

    New languages added

--- Frontend/src/components/02_code_section/02.1_CodeLangSelect.jsx
+++ Frontend/src/components/02_code_section/02.1_CodeLangSelect.jsx
@@ -28,6 +28,56 @@ function CodeLangSelect({ selectedLang, setSelectedLang }) {
         'SQL': 'sql',
         'MATLAB': 'matlab',
         'Groovy': 'groovy',
+        'Assembly': 'assembly',
+        'Visual Basic .NET': 'vbnet',
+        'F#': 'fsharp',
+        'Clojure': 'clojure',
+        'Erlang': 'erlang',
+        'Fortran': 'fortran',
+        'COBOL': 'cobol',
+        'Julia': 'julia',
+        'OCaml': 'ocaml',
+        'Nim': 'nim',
+        'Zig': 'zig',
+        'Crystal': 'crystal',
+        'Solidity': 'solidity',
+        'VHDL': 'vhdl',
+        'Verilog': 'verilog',
+        'Ada': 'ada',
+        'Prolog': 'prolog',
+        'Lisp': 'lisp',
+        'Common Lisp': 'commonlisp',
+        'Scheme': 'scheme',
+        'Tcl': 'tcl',
+        'AWK': 'awk',
+        'Sed': 'sed',
+        'Bash': 'bash',
+        'Fish': 'fish',
+        'Smalltalk': 'smalltalk',
+        'Pascal': 'pascal',
+        'Delphi': 'delphi',
+        'Apex': 'apex',
+        'ABAP': 'abap',
+        'LabVIEW': 'labview',
+        'SAS': 'sas',
+        'PL/SQL': 'plsql',
+        'PostgreSQL': 'postgresql',
+        'GraphQL': 'graphql',
+        'Elm': 'elm',
+        'ReasonML': 'reasonml',
+        'Racket': 'racket',
+        'Standard ML': 'sml',
+        'Mercury': 'mercury',
+        'Io': 'io',
+        'Pony': 'pony',
+        'Hack': 'hack',
+        'Monkey C': 'monkeyc',
+        'Q#': 'qsharp',
+        'Logo': 'logo',
+        'ActionScript': 'actionscript',
+        'AutoHotkey': 'autohotkey',
+        'OpenCL': 'opencl',
+        'CUDA': 'cuda'
     }
 
     return (
@@ -35,8 +85,8 @@ function CodeLangSelect({ selectedLang, setSelectedLang }) {
             <select
                 value={selectedLang}
                 onChange={(e) => setSelectedLang(e.target.value)}
-                className=
-                    "appearance-none px-4 py-2 pr-10 rounded-lg 
+                className =
+                    "appearance-none px-4 py-2 pr-10 rounded-lg
                     bg-white/10 backdrop-blur-md text-white 
                     border border-white/20 shadow-md 
                     focus:outline-none focus:ring-2 focus:ring-blue-500/50 
@@ -52,7 +102,7 @@ function CodeLangSelect({ selectedLang, setSelectedLang }) {
 
             {/* Custom Arrow */}
             <div className="pointer-events-none absolute inset-y-0 right-3 flex items-center text-gray-300">
-                ▼
+
             </div>
             
         </div>
